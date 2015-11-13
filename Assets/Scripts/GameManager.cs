02f897505da30d2466466831d6480531fddb83cd
Vladislavs Marisevs <vmarisevs@gmail.com>
2015-11-13 02:40:51 +0000

    fixing 2D (mistake while creating Unity project was defined as 3D)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    // allows to create only one instance of this class
+    public static GameManager instance = null;
+
+    // map generation script
+    public MapManager mapScript;
+
+    // game init
+    void InitGame()
+    {
+        // clear from enemies
+        // generate by level difficulty
+        mapScript.SetupScene();
+    }
+
+
+    // on awake 
+    void Awake()
+    {
+        // blocking to load more instances of this class
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        // running init scripts
+        mapScript = GetComponent<MapManager>();
+        InitGame();
+    }
+
+
+    public void GameOver()
+    {
+        enabled = false;
+    }
+
+
+
+}
---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class MapManager : MonoBehaviour {
+
+    // public variables
+    public int columns = 16;
+    public int rows = 8;
+    // public object arrays
+    public GameObject[] groundTiles;
+
+
+    // private variables
+    private Transform mapHolder;
+
+
+    // buld the map
+    private void MapSetup()
+    {
+        // init map object
+        mapHolder = new GameObject("Map").transform;
+
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                // generating random ground tile
+                GameObject toInstantiate = groundTiles[Random.Range(0, groundTiles.Length)];
+
+                // applying position
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+                // adding into parent
+                instance.transform.SetParent(mapHolder);
+            }
+        }
+
+    }
+
+	// generates the scene
+    public void SetupScene()
+    {
+        // generating the map
+        MapSetup();
+
+    }
+}
