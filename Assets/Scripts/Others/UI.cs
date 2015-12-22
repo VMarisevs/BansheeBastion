c672a2c971ec75ffa697ad9b68ebdf78cb9de1e5
Vladislavs Marisevs <vmarisevs@gmail.com>
2015-12-22 17:29:50 +0000

    default menu

--- Assets/Scripts/Others/UI.cs
+++ Assets/Scripts/Others/UI.cs
@@ -4,22 +4,58 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour {
-    private SpriteRenderer buttonSr;
+    //private SpriteRenderer buttonSr;
 
-    private Sprite[] uiSprites;
+    //private Sprite[] uiSprites;
 
-    public void Awake()
-    {
-        uiSprites = Resources.LoadAll<Sprite>("Sprites/UI");
-       // print(uiSprites[0]);
-        print(uiSprites.Length);
-    }
+    //public void Awake()
+    //{
+    //   // uiSprites = Resources.LoadAll<Sprite>("Sprites/UI");
+    //   //// print(uiSprites[0]);
+    //   // print(uiSprites.Length);
+    //}
 
-
-
+    public GameObject[] menuCanvas;
+    //public bool[] menu = new bool[] { true, false }; 
+            
     public void LoadScene(int level)
     {
         SceneManager.LoadScene(level);
     }
 
+    public void _openScoresTab()
+    {
+        changeMenu(1);
+    }
+
+    public void _openSettingsTab()
+    {
+        changeMenu(2);
+    }
+
+    public void _openHelpTab()
+    {
+        changeMenu(3);
+    }
+
+    public void _openMainTab()
+    {
+        changeMenu(0);
+    }
+
+    private void changeMenu(int id)
+    {
+        for (int i = 0; i < menuCanvas.Length; i++)
+        {
+            menuCanvas[i].SetActive(false);
+        }
+
+        menuCanvas[id].SetActive(true);
+    }
+
+
+    public void Start()
+    {
+       _openMainTab();
+    }
 }
